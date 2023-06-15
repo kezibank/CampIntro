74fc915a32f70ba5e250d634e1d8150df69d9630
Keziban Karabaş <41192933+kezibank@users.noreply.github.com>
2023-06-16 00:35:41 +0300

    Add files via upload

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Keziban Karabaş";
+            kurs1.IzlenmeOrani = 76;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Python";
+            kurs2.Egitmen = "Şeymanur Güney";
+            kurs2.IzlenmeOrani = 85;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "C++";
+            kurs3.Egitmen = "Dursun Karabaş";
+            kurs3.IzlenmeOrani = 66;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (Kurs kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " " + kurs.Egitmen);        
+            }
+          
+        }
+    }
+}
+class Kurs
+{
+    public string KursAdi { get; set; }
+    public string Egitmen { get; set; }
+    public int IzlenmeOrani { get; set; }
+
+}
